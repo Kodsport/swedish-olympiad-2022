cee6f58404d953a378bd45ebf2cfcf02d614863a
Nils Gustafsson <nilsgust@kth.se>
2021-10-20 17:12:14 +0200

    [kortavokaler] add problem, no statement yet

---
+++ skolkval/kortavokaler/input_format_validators/validator/validator.cpp
@@ -0,0 +1,13 @@
+#include "validator.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+
+void run() {
+	string s = Line();
+}
