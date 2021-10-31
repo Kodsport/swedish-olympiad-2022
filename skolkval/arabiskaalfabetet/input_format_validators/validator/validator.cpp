7f0ff429db372573141fd8eb43bccde31a83b7df
Joakim Blikstad <jockemocke@gmail.com>
2021-10-31 13:26:37 +0100

    [arabiskaalphabetet] Add solution and fix validator

--- skolkval/arabiskaalfabetet/input_format_validators/validator/validator.cpp
+++ skolkval/arabiskaalfabetet/input_format_validators/validator/validator.cpp
@@ -9,7 +9,17 @@ using namespace std;
 typedef long long ll;
 
 void run() {
-	int n= Int(1,10);
-	Endl();
-	string s = Line();
+  int n = Int(1,10);
+  Endl();
+  string ok_chars = ".,?!_";
+  rep(i,0,n) {
+    string s = _token();
+    if(sz(s) == 0) die_line("empty word");
+    for(char c:s)
+      if(!(isalpha(c) || count(all(ok_chars),c)))
+        die_line("invalid char in word \"" + s +"\"");
+    if(i == n-1) Endl();
+    else Space();
+  }
+  Eof();
 }
