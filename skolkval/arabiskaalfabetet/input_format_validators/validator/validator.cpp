6ac3bd5a2a688ad65de4e64ce960fe3bf0376d74
abdzag <abdzag@DESKTOP-K1Q1H38.localdomain>
2021-11-03 22:20:32 +0100

    [arabiskaalfabetet] validator now works for subtask

--- skolkval/arabiskaalfabetet/input_format_validators/validator/validator.cpp
+++ skolkval/arabiskaalfabetet/input_format_validators/validator/validator.cpp
@@ -2,24 +2,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define trav(a, x) for (auto &a : x)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
 typedef long long ll;
 
-void run() {
-  int n = Int(1,10);
+void run()
+{
+  int n = Int(1, 10);
   Endl();
+  bool subtask = Arg("subtask", 0);
+  string vowels = "aeiouyAEIOUY";
+  string consonants = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ";
   string ok_chars = ".,?!_";
-  rep(i,0,n) {
+  rep(i, 0, n)
+  {
     string s = _token();
-    if(sz(s) == 0) die_line("empty word");
-    for(char c:s)
-      if(!(isalpha(c) || count(all(ok_chars),c)))
-        die_line("invalid char in word \"" + s +"\"");
-    if(i == n-1) Endl();
-    else Space();
+    if (sz(s) == 0)
+      die_line("empty word");
+    for (int j = 0; j < s.size(); j++)
+    {
+      char c = s[j];
+      if (!(isalpha(c) || count(all(ok_chars), c)))
+        die_line("invalid char in word \"" + s + "\"");
+      if (subtask && j + 2 < s.size() && count(all(vowels), c) && count(all(consonants), s[j + 1]) && count(all(consonants), s[j + 2]))
+        die_line("short vowel included in subtask");
+    }
+    if (i == n - 1)
+      Endl();
+    else
+      Space();
   }
   Eof();
 }
