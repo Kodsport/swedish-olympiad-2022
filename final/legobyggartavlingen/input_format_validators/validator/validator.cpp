6d9d794a261575ac322842098961877f16f16015
Simon Lindholm <simon.lindholm10@gmail.com>
2022-01-19 16:24:28 +0100

    [legobyggartavlingen] input validator

--- final/legobyggartavlingen/input_format_validators/validator/validator.cpp
+++ final/legobyggartavlingen/input_format_validators/validator/validator.cpp
@@ -1,41 +1,44 @@
 #include "validator.h"
 
-#include <bits/stdc++.h>
-using namespace std;
-
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(i, a) for(auto& i : a)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-
-
 void run() {
-  int maxn = Arg("maxn");
-  int k = 3;
-  int same = Arg("same", 0);
+	int a = Int(0, Arg("a"));
+	Space();
+	int b = Int(0, Arg("b"));
+	Space();
+	int m = Int(0, Arg("m"));
+	Endl();
 
-  int n = Int(3, maxn);
-  int maxa = Arg("maxa", n);
-  Endl();
+	vector<pair<int, int>> towers, masts;
+	int towerMinX = INT_MAX;
+	int mastMaxX = INT_MIN;
+	for (int i = 0; i < a + b; i++) {
+		int x = Int(0, 1'000'000'000);
+		Space();
+		int f = Int(0, 1000);
+		Space();
+		int h = Int(0, 1000);
+		Endl();
+		towers.push_back({x,h});
+		towerMinX = min(towerMinX, x);
+	}
 
-  int a0;
-  rep(c1,0,n){
-      int a = Int(1, maxa);
+	for (int i = 0; i < m; i++) {
+		int x = Int(0, 1'000'000'000);
+		Space();
+		int h = Int(0, 1000);
+		Endl();
+		masts.push_back({x,h});
+		mastMaxX = max(mastMaxX, x);
+	}
 
-        if(c1 == 0){
-            a0 = a;
-        }
-        else{
-            if(same == 1)assert(a == a0);
-        }
+	for (auto tow : towers) {
+		for (auto ma : masts) {
+			int dx = abs(tow.first - ma.first);
+			assert(tow.second <= ma.second - dx);
+		}
+	}
 
-      if(c1 < n-1)Space();
-  }
-  Endl();
-  Eof();
-
+	if (Arg("separated")) {
+		assert(mastMaxX < towerMinX);
+	}
 }
-
