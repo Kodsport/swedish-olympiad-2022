cb4d9baefa2f9f63a139b58cda8101810c5e063e
Simon Lindholm <simon.lindholm10@gmail.com>
2022-01-20 00:09:19 +0100

    [legobyggartavlingen] update constraints

--- final/legobyggartavlingen/input_format_validators/validator/validator.cpp
+++ final/legobyggartavlingen/input_format_validators/validator/validator.cpp
@@ -1,35 +1,42 @@
 #include "validator.h"
 
 void run() {
-	int a = Int(0, Arg("a"));
+	int abmLim = Arg("abm");
+	int a = Int(1, abmLim);
 	Space();
-	int b = Int(0, Arg("b"));
+	int b = Int(1, abmLim);
 	Space();
-	int m = Int(0, Arg("m"));
+	int m = Int(1, abmLim);
 	Endl();
 
 	vector<pair<int, int>> towers, masts;
 	int towerMinX = INT_MAX;
 	int mastMaxX = INT_MIN;
+	set<int> towersX;
 	for (int i = 0; i < a + b; i++) {
-		int x = Int(0, 1'000'000'000);
+		int x = Int(1, 1'000'000);
 		Space();
-		int f = Int(0, 1000);
+		int f = Int(1, 100);
 		Space();
-		int h = Int(0, 1000);
+		int h = Int(1, 10000);
 		Endl();
 		towers.push_back({x,h});
 		towerMinX = min(towerMinX, x);
+		towersX.insert(x);
 	}
+	assert((int)towersX.size() == a + b);
 
+	set<int> mastsX;
 	for (int i = 0; i < m; i++) {
-		int x = Int(0, 1'000'000'000);
+		int x = Int(1, 1'000'000);
 		Space();
-		int h = Int(0, 1000);
+		int h = Int(1, 10000);
 		Endl();
 		masts.push_back({x,h});
+		mastsX.insert(x);
 		mastMaxX = max(mastMaxX, x);
 	}
+	assert((int)mastsX.size() == m);
 
 	for (auto tow : towers) {
 		for (auto ma : masts) {
